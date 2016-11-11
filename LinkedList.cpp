2860a2886209b46b1de0faa2361b01f15880ed86
Alexis Orozco <alext-97@live.cl>
2016-11-10 23:34:08 -0300

    Se agrega: Mostrar lista, verificar exstencia en la lista

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,28 +2,48 @@
 
 using namespace std;
 
-struct node
-{
+struct node{
     int value;
     node *next;
 };
 
 
-void add_node( node *ptr, int value  )
-{
-    node *new_node = new node;
-    new_node -> value =  value;
-    new_node -> next = (ptr -> next);
-    ptr -> next = new_node;
+void add_node(node *ptr, int value){ //Inserta un nodo despues del head
+    node *new_node = new node; //Crea el nuevo nodo
+    new_node -> value = value; //Le agrega el valor al nuevo nodo
+    new_node -> next = (ptr -> next); //Pone en el next del nuevo nodo, el next del nodo que habia antes
+    ptr -> next = new_node; //El next del anterior ahora apunta al nuevo noda
 }
 
-int main()
-{
+void list_nodes(node *ptr){
+    node *actual; //Crea un nodo auxiliar "actual"
+    actual = ptr; //Actual, sera el nodo HEAD 
+    while(actual!=NULL){ //Recorrera los nodos hasta que sea NULL
+        cout << actual->value << endl; //Muestra el ->valor del nodo actual
+        actual = actual->next; //Actual ahora sera el siguiente nodo
+    }
+}
+
+void buscar_nodes(node *ptr, int buscado){
+    node *actual; //Nodo aux
+    actual = ptr; //Primero sera el HEAD
+    while(actual != NULL){ //recorre la lista
+        if(actual->value == buscado){ 
+            cout << "Coincidencia encontrada!" << endl;
+        }
+        actual = actual->next; //Avanza el la lista
+    }
+}
+
+int main(){
     node *ptr;
-    ptr = new node;
-    ptr -> value = 1;
-    ptr -> next = NULL;
-    add_node(ptr, 2);
-    cout << ptr->value << ptr->next->value<<endl;
+    ptr = new node; //Crea un nodo
+    ptr -> value = 1; //Le asigna el valor 1 al primer nodo
+    ptr -> next = NULL; //Deja el next hacia NULL
+    add_node(ptr, 34);
+    add_node(ptr, 56);
+    add_node(ptr, 78);
+    list_nodes(ptr);
+    buscar_nodes(ptr, 34);
     return 0;
 }
