d48fe21857ceb834274c27d1a8c83d5a1f6d5959
Alexis Orozco <alext-97@live.cl>
2016-11-11 00:00:54 -0300

    Se agrega: Agregar al final, Obtener indice de una busqueda, cambios menores a las funciones anteriores

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -16,8 +16,7 @@ void add_node(node *ptr, int value){ //Inserta un nodo despues del head
 }
 
 void list_nodes(node *ptr){
-    node *actual; //Crea un nodo auxiliar "actual"
-    actual = ptr; //Actual, sera el nodo HEAD 
+    node *actual = ptr; //Crea un nodo auxiliar "actual" //Actual, sera el nodo HEAD 
     while(actual!=NULL){ //Recorrera los nodos hasta que sea NULL
         cout << actual->value << endl; //Muestra el ->valor del nodo actual
         actual = actual->next; //Actual ahora sera el siguiente nodo
@@ -25,14 +24,40 @@ void list_nodes(node *ptr){
 }
 
 void buscar_nodes(node *ptr, int buscado){
-    node *actual; //Nodo aux
-    actual = ptr; //Primero sera el HEAD
+    node *actual = ptr; //Nodo aux //Primero sera el HEAD
     while(actual != NULL){ //recorre la lista
         if(actual->value == buscado){ 
             cout << "Coincidencia encontrada!" << endl;
         }
         actual = actual->next; //Avanza el la lista
     }
+}
+
+void get_indice(node *ptr, int buscado){
+	node *actual = ptr;
+	int indx = 0;
+	while(actual != NULL){
+		if (actual->value == buscado){
+			cout << "El int buscado (" << buscado << ") se encuentra en el indice " << indx << endl;
+		}
+		indx++;
+		actual = actual->next;
+	}
+}
+
+void add_node_final(node *ptr, int valor){
+	node *nuevo = new node; //Se crea el nuevo nodo
+	node *actual = ptr; //Se crea nodo aux para iterar
+	node *ultimo = new node; //Se crea nodo aux para saber cual es el ultimo
+	nuevo->value = valor; //Se le asgina el valor al nuevo
+	nuevo->next = NULL; //Como esta al final, el valor es NULL
+	while(actual != NULL){
+		if(actual->next == NULL){ //Si es que es el ultimo de la lista
+			ultimo = actual; //Se quiere que se convierta en el otro auxiliar
+		}
+		actual = actual->next;
+	}
+	ultimo->next = nuevo; //para finalmente agregar el nuevo al final de la lista
 }
 
 int main(){
@@ -43,7 +68,10 @@ int main(){
     add_node(ptr, 34);
     add_node(ptr, 56);
     add_node(ptr, 78);
+    add_node_final(ptr,233);
+    add_node_final(ptr,5234);
     list_nodes(ptr);
     buscar_nodes(ptr, 34);
+    get_indice(ptr, 5234);
     return 0;
 }
